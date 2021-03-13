8796d70607bdf8c12b87fe11790669c34c6f804b
Rajat Dhamija <rajat@appyhigh.com>
2021-03-13 18:47:37 +0530

    changed installActivity to install in myApplication

--- app/src/main/java/me/rajatdhamija/dynamicmodule/MyApplication.kt
+++ app/src/main/java/me/rajatdhamija/dynamicmodule/MyApplication.kt
@@ -1,6 +1,5 @@
 package me.rajatdhamija.dynamicmodule
 
-import android.app.Application
 import android.content.Context
 import com.google.android.play.core.splitcompat.SplitCompat
 import com.google.android.play.core.splitcompat.SplitCompatApplication
@@ -8,6 +7,6 @@ import com.google.android.play.core.splitcompat.SplitCompatApplication
 class MyApplication : SplitCompatApplication() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        SplitCompat.installActivity(this)
+        SplitCompat.install(this)
     }
 }
